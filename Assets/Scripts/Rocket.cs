9e4dfa1e2aaa848aed76768785095f9839cffc69
Sont3k <markus.residence@gmail.com>
2020-03-10 14:27:14 +0200

    44. Physics and Rigidbodies

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -2,32 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rocket : MonoBehaviour 
+public class Rocket : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start () 
-    {
+    Rigidbody rigidBody;
 
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update () 
+    void Update()
     {
-        ProcessInput ();
+        ProcessInput();
     }
 
-    private void ProcessInput () 
+    private void ProcessInput()
     {
-        if(Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
         {
+            rigidBody.AddRelativeForce(Vector3.up);
             print("Thrusting");
         }
 
-        if(Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
         {
             print("Rotating left");
         }
-        else if(Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
             print("Rotating right");
         }
